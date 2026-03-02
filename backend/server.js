7d467c09a155f0e1f952deebe55df1be08fef296
Harika-44 <seesaharika@gmail.com>
2026-03-02 22:12:48 +0530

    updated profile controller

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import paymentsRoutes from "./routes/payments.routes.js";
 import AuthRoutes from "./routes/auth.routes.js"
-
+import profileRouter from "./routes/profile.routes.js"
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1/payments", paymentsRoutes);
 app.use("/api/v1/auth", AuthRoutes);
-
+app.use("/api/profile", profileRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
